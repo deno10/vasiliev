1fb0bcddc2f976f76e6cd58b55e495d15327cf62
deno10 <deno10@yandex.ru>
2021-03-26 19:48:37 +0300

    publication adaptive

--- publication.php
+++ publication.php
@@ -211,6 +211,25 @@ if (!$row) header("Location: index.php");
 				</div>
 			</div>
 		</div>
+		<div class="navup_mobile">
+			<div class="navup_mobile_wrapper">
+				<div class="navup_mobile_outer">
+					<div class="navup_mobile_inner">
+						<div class="navup_mobile_arrow">
+							<a class="navup_mobile_arrow_link" href="index.html">
+								<span class="navup_mobile_arrow_wrapper">
+									<svg class="navup_mobile_arrow_svg" fill="#262626" height="24" width="24" viewBox="0 0 48 48">
+									<path d="M40 33.5c-.4 0-.8-.1-1.1-.4L24 18.1l-14.9 15c-.6.6-1.5.6-2.1 0s-.6-1.5 0-2.1l16-16c.6-.6 1.5-.6 2.1 0l16 16c.6.6.6 1.5 0 2.1-.3.3-.7.4-1.1.4z"></path>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<h1 class="navup_mobile_caption">Фото</h1>
+						<div class="navup_mobile_filler"></div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="footer"></div>
 	</body>
 </html>
